a904f760742ecacc4df34571f14f5ed99b1ae093
kazu <180441147@ccmailg.meijo-u.ac.jp>
2020-10-21 15:44:27 +0900

    settings.pyの修正

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ conf = configparser.ConfigParser()
 conf.read('settings.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
